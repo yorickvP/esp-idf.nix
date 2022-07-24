90b4ef86eb05692bdceeef39de4af9585646144b
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-07-24 12:41:17 +0200

    flake: make overlay

--- flake.nix
+++ flake.nix
@@ -5,13 +5,31 @@
   inputs.mach-nix.url = "github:DavHau/mach-nix";
   inputs.mach-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, mach-nix }: {
+  outputs = { self, nixpkgs, mach-nix }: let
+    eachSystem = nixpkgs.lib.genAttrs ["x86_64-linux"];
+  in {
+    overlays.default = (self: super: {
+      mach-nix = import mach-nix {
+        pkgs = self;
+        pypiData = mach-nix.inputs.pypi-deps-db;
+      };
+      esp-idf = super.callPackage ./package.nix {
+        mkPython = self.mach-nix.mkPython;
+      };
+    });
+    
+    packages = eachSystem (system: let
+      pkgs = import nixpkgs {
+        overlays = [self.overlays.default];
+        inherit system;
+      };
+    in {
+      inherit (pkgs) esp-idf;
+      default = pkgs.esp-idf;
+    });
 
-    packages.x86_64-linux.esp-idf = nixpkgs.legacyPackages.x86_64-linux.callPackage ./package.nix {
-      mkPython = mach-nix.lib.x86_64-linux.mkPython;
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [self.packages.x86_64-linux.esp-idf];
     };
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.esp-idf;
-
   };
 }

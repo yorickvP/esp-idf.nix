4c85d80545e66522a1f5442af2d8bd9d0bdeda66
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-07-24 13:03:33 +0200

    flake: export legacyPackages, fmt

--- flake.nix
+++ flake.nix
@@ -5,31 +5,35 @@
   inputs.mach-nix.url = "github:DavHau/mach-nix";
   inputs.mach-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, mach-nix }: let
-    eachSystem = nixpkgs.lib.genAttrs ["x86_64-linux"];
-  in {
-    overlays.default = (self: super: {
-      mach-nix = import mach-nix {
-        pkgs = self;
-        pypiData = mach-nix.inputs.pypi-deps-db;
-      };
-      esp-idf = super.callPackage ./package.nix {
-        mkPython = self.mach-nix.mkPython;
-      };
-    });
-    
-    packages = eachSystem (system: let
-      pkgs = import nixpkgs {
-        overlays = [self.overlays.default];
-        inherit system;
-      };
+  outputs = { self, nixpkgs, mach-nix }:
+    let eachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
     in {
-      inherit (pkgs) esp-idf;
-      default = pkgs.esp-idf;
-    });
+      overlays.default = (self: super: {
+        mach-nix = import mach-nix {
+          pkgs = self;
+          pypiData = mach-nix.inputs.pypi-deps-db;
+        };
+        esp-idf = super.callPackage ./package.nix {
+          mkPython = self.mach-nix.mkPython;
+        };
+      });
 
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [self.packages.x86_64-linux.esp-idf];
+      legacyPackages = eachSystem (system:
+        import nixpkgs {
+          overlays = [ self.overlays.default ];
+          config = { };
+          inherit system;
+        });
+      packages = eachSystem (system:
+        let pkgs = self.legacyPackages.${system};
+        in {
+          inherit (pkgs) esp-idf;
+          default = pkgs.esp-idf;
+        });
+
+      #     devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      # IDF_PATH = "${esp-idf}";
+      #       buildInputs = [self.packages.x86_64-linux.esp-idf];
+      #     };
     };
-  };
 }
